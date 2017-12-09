2afc6b008a4793e00a4ff045c101f360c484fca9
Kirill Shepelev <kirill.shepelev.mail@gmail.com>
2017-12-09 20:58:51 +0300

    Changed exceptions

--- server/exceptions.py
+++ server/exceptions.py
@@ -1,11 +1,25 @@
-class DeviceException(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+from enum import IntEnum
+from flask import jsonify
 
-class UserException(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+class ErrorCode(IntEnum):
+    NOT_OWNER = 0
+    DEVICE_NOT_REGISTERED = 1
+    DATA_NOT_SUITABLE = 2
+    OTHER_ERROR = 3
+    INVALID_DEVICE_TOKEN = 4
 
-class OwnerException(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+
+class ServerErrorException(Exception):
+    def __init__(self, code=4, json=None, message=None, *args):
+        Exception.__init__(self, *args)
+        if json is None:
+            json = dict()
+        self.json = json
+        self.json['error_message'] = message
+        self.json['error_code'] = code
+
+    def get_json(self):
+        return jsonify(self.json)
+
+    def get_dict(self):
+        return self.json
